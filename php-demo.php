ab885ebe2ccb4d62aeb84aeb0321c62dab581455
lyx <1243160287@qq.com>
2019-12-23 10:25:06 +0800

    update

--- php-demo.php
+++ php-demo.php
@@ -9,9 +9,9 @@ function activate($vendor_sn, $vendor_key)
     $api_domain = 'https://api.shouqianba.com';
     $url = $api_domain .'/terminal/activate';
 
-    $params['app_id'] = '2019******001786';    //app id，从服务商平台获取2017112500000439
-    $params['code'] = '74649220';              //激活码内容11654978
-    $params['device_id'] = '5210000010011';                 //设备唯一身份ID
+    $params['app_id'] = '';    //app id，从服务商平台获取
+    $params['code'] = '';              //激活码内容
+    $params['device_id'] = '';                 //设备唯一身份ID
 
     $ret = pre_do_execute($params, $url, $vendor_sn, $vendor_key);
 
@@ -28,7 +28,7 @@ function checkin($terminal_sn, $terminal_key)
     $url = $api_domain . '/terminal/checkin';
 
     $params['terminal_sn'] = $terminal_sn;       //终端号
-    $params['device_id'] = '2019-07-03 09:49';                //设备唯一身份ID
+    $params['device_id'] = '';                //设备唯一身份ID
 
 
     $ret = pre_do_execute($params, $url, $terminal_sn, $terminal_key);
@@ -44,21 +44,21 @@ function precreate($terminal_sn, $terminal_key)
     $url = $api_domain . '/upay/v2/precreate';
 
     $params['terminal_sn'] = $terminal_sn;      //收钱吧终端ID
-    //$params['sn']='7895253130995555';         //收钱吧系统内部唯一订单号
-    $params['client_sn'] = '1231ksjljkj';  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
-    $params['total_amount'] = '1';             //交易总金额
-    $params['payway']='1';
-    $params['subject'] = 'pizza';              //本次交易的概述
-    $params['operator'] = 'Obama';             //发起本次交易的操作员
-    $params['sub_payway']='6';           //内容为数字的字符串，如果要使用WAP支付，则必须传 "3", 使用小程序支付请传"4"
-    //$params['payer_uid']='kun565045067';          //消费者在支付通道的唯一id,微信WAP支付必须传open_id,支付宝WAP支付必传用户授权的userId
+    //$params['sn']='';         //收钱吧系统内部唯一订单号
+    $params['client_sn'] = '';  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+    $params['total_amount'] = '';             //交易总金额
+    $params['payway']='';
+    $params['subject'] = '';              //本次交易的概述
+    $params['operator'] = '';             //发起本次交易的操作员
+    $params['sub_payway']='';           //内容为数字的字符串，如果要使用WAP支付，则必须传 "3", 使用小程序支付请传"4"
+    //$params['payer_uid']='';          //消费者在支付通道的唯一id,微信WAP支付必须传open_id,支付宝WAP支付必传用户授权的userId
     //$params['description']='';           //对商品或本次交易的描述
     //$params['longitude']='';             //经纬度必须同时出现
     //$params['latitude']='';              //经纬度必须同时出现
     //$params['extended']='';              //收钱吧与特定第三方单独约定的参数集合,json格式，最多支持24个字段，每个字段key长度不超过64字节，value长度不超过256字节
     //$params['goods_details']='';         //商品详情
     //$params['reflect']='';               //任何调用者希望原样返回的信息
-    //$params['notify_url']='www.baidu.com';     //支付回调的地址
+    //$params['notify_url']='';     //支付回调的地址
     $ret = pre_do_execute($params, $url, $terminal_sn, $terminal_key);
     return $ret ;
 }
@@ -73,11 +73,11 @@ function  pay($terminal_sn, $terminal_key)
 
     $params['terminal_sn'] = $terminal_sn;              //终端号
     $params['client_sn'] = getClient_Sn(16); //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
-    $params['total_amount'] = '10000';                      //交易总金额,以分为单位
+    $params['total_amount'] = '';                      //交易总金额,以分为单位
     //$params['payway'] = '1';                          //支付方式,1:支付宝 3:微信 4:百付宝 5:京东钱包
     $params['dynamic_id'] = '';       //条码内容（支付包或微信条码号）
-    $params['subject'] = 'pizz';                        //交易简介
-    $params['operator'] = 'kun';                        //门店操作员
+    $params['subject'] = '';                        //交易简介
+    $params['operator'] = '';                        //门店操作员
 
     //$params['description']='';                        //对商品或本次交易的描述
     //$params['longitude']='';                          //经度(经纬度必须同时出现)
@@ -86,7 +86,7 @@ function  pay($terminal_sn, $terminal_key)
     //$params['extended']='';                           //扩展参数集合  { "goods_tag": "beijing"，"goods_id":"1"}
     //$params['goods_details']='';                      //商品详情 goods_details": [{"goods_id": "wx001","goods_name": "苹果笔记本电脑","quantity": 1,"price": 2,"promotion_type": 0}]
     //$params['reflect']='';                            //反射参数
-    //$params['notify_url']='';                         //支付回调地址(www.baidu.com 如果支付成功通知时间间隔为1s,5s,30s,600s)
+    //$params['notify_url']='';                         //支付回调地址(如果支付成功通知时间间隔为1s,5s,30s,600s)
 
     $ret = pre_do_execute($params, $url, $terminal_sn, $terminal_key);
     return $ret;
@@ -101,12 +101,12 @@ function refund($terminal_sn, $terminal_key)
     $url = $api_domain . '/upay/v2/refund';
 
     $params['terminal_sn'] = $terminal_sn;       //收钱吧终端ID
-    $params['sn'] = '7895239974544350';        //收钱吧系统内部唯一订单号（N）
-    //$params['client_sn']='Se12345678';   //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
-    //$params['client_tsn']='S12345676';  //商户退款流水号一笔订单多次退款，需要传入不同的退款流水号来区分退款，如果退款请求超时，需要发起查询，并根据查询结果的client_tsn判断本次退款请求是否成功
-    $params['refund_amount'] = '10000';              //退款金额
-    $params['refund_request_no'] = '001';        //商户退款所需序列号,表明是第几次退款(正常情况不可重复，意外状况爆出不变)
-    $params['operator'] = 'kun';                 //门店操作员
+    $params['sn'] = '';        //收钱吧系统内部唯一订单号（N）
+    //$params['client_sn']='';   //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+    //$params['client_tsn']='';  //商户退款流水号一笔订单多次退款，需要传入不同的退款流水号来区分退款，如果退款请求超时，需要发起查询，并根据查询结果的client_tsn判断本次退款请求是否成功
+    $params['refund_amount'] = '';              //退款金额
+    $params['refund_request_no'] = '';        //商户退款所需序列号,表明是第几次退款(正常情况不可重复，意外状况爆出不变)
+    $params['operator'] = '';                 //门店操作员
     //$params['extended'] = '';                    //扩展参数集合
     //$params['goods_details'] = '';               //商品详情
 
@@ -122,8 +122,8 @@ function query($terminal_sn, $terminal_key)
     $url = $api_domain . '/upay/v2/query';
 
     $params['terminal_sn'] = $terminal_sn;      //收钱吧终端ID
-    $params['sn']='7895239328199504';         //收钱吧系统内部唯一订单号
-    //$params['client_sn'] = '652110026320';    //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+    $params['sn']='';         //收钱吧系统内部唯一订单号
+    //$params['client_sn'] = '';    //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
 
     $ret = pre_do_execute($params, $url, $terminal_sn, $terminal_key);
     return $ret;
@@ -134,13 +134,13 @@ function query($terminal_sn, $terminal_key)
 function wap_api_pro($terminal_sn, $terminal_key)
 {
     $params['terminal_sn'] = $terminal_sn;     //收钱吧终端ID
-    $params['client_sn'] = '932409283402384';    //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
-    $params['total_amount'] = '1';             //以分为单位,不超过10位纯数字字符串,超过1亿元的收款请使用银行转账
-    $params['subject'] = '披萨';              //本次交易的概述
+    $params['client_sn'] = '';    //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+    $params['total_amount'] = '';             //以分为单位,不超过10位纯数字字符串,超过1亿元的收款请使用银行转账
+    $params['subject'] = '';              //本次交易的概述
     //$params['payway']='1';
-    $params['notify_url'] = 'www.shouqianba.com';   //支付回调的地址
-    $params['operator'] = 'Obama';                    //发起本次交易的操作员
-    $params['return_url'] = "http://www.baidu.com";  //处理完请求后，当前页面自动跳转到商户网站里指定页面的http路径
+    $params['notify_url'] = '';   //支付回调的地址
+    $params['operator'] = '';                    //发起本次交易的操作员
+    $params['return_url'] = "";  //处理完请求后，当前页面自动跳转到商户网站里指定页面的http路径
 
     ksort($params);  //进行升序排序
 
